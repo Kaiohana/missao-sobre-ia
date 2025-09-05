c12b4805707f66ad9c47d8404b9861558dc2173f
Kaiohana <162453614+Kaiohana@users.noreply.github.com>
2025-09-05 11:04:30 -0300

    m

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const perguntas = [
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso é augo impressonante!",
+                afirmacao: "No início ficou encantada com que essa tecnologia pode fazer. "
             },
             {
                 texto: "Isso é maravilhoso!",
